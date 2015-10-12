f7bdeb624b86b252eff88e1b6996f10f020fa533
Ricardo Delfin <ricardo.delfin.garcia@gmail.com>
2015-10-12 15:02:19 -0500

    Created StringParser abstract class that extends parser abstract class.

--- include/black-widow/Parser.h
+++ include/black-widow/Parser.h
@@ -1,5 +1,5 @@
 //
-// Created by ricar on 10/12/2015.
+// Created by Ricardo Delfin Garcia on 10/12/2015.
 //
 
 #ifndef BLACK_WIDOW_PARSER_H
@@ -9,13 +9,27 @@
 #include <vector>
 
 namespace bw {
+    /**
+     * Defines a generic stream parser that tokenizes the data in some form or fashion. Eases parsing incomming HTTP
+     * response data. The data will be stored in a vector<T> data structure. Therefore, the T represents the type of
+     * data that a parser results in.
+     */
     template <class T>
     class Parser {
     public:
+        /**
+         * Default constructor. Uses a data stream as an input.
+         */
         Parser(std::istream& stream) : stream(stream) { }
 
+        /**
+         * Parses the data and returns a std::vector itterator. THe
+         */
         virtual std::vector<T>::iterator& parse() = 0;
 
+        /**
+         * Default destructor does nothing
+         */
         virtual ~Parser() { }
     private:
 
